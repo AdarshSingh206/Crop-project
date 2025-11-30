027f29ca67736600e4484f0968dec47032226b61
AdarshSingh206 <krishsingh76404@gmail.com>
2025-11-30 12:55:57 +0530

    Update video source path and container height

--- video.jsx
+++ video.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 const video = () => {
   return (
-    <div className='h-full w-full'>
-         <video className='h-full w-full object-cover' autoPlay muted loop src="https://www.pexels.com/download/video/3783928/"></video>
+    <div className='h-screen w-full'>
+         <video className='h-full w-full object-cover' autoPlay muted loop src="/videos/crop.mp4"></video>
     </div>
   )
 }
 
 export default video
+
